15c8e6bdf6f4027f7cc00318a7b2968b60075295
Gauthier Bordat <gauthier.bordat@gmail.com>
2020-11-28 17:02:31 +0100

    petit commit pck on a fait pas mal de modif

---
+++ android/app/src/main/java/com/ismin/projectapp/LigneAdapter.kt
@@ -0,0 +1,20 @@
+package com.ismin.projectapp
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class LigneAdapter(private val lignes: ArrayList<Ligne>) : RecyclerView.Adapter<LigneViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LigneViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: LigneViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}
---
+++ android/app/src/main/java/com/ismin/projectapp/LignelistFragment.kt
@@ -0,0 +1,42 @@
+package com.ismin.projectapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+private const val ARG_LIGNES = "ARG_LIGNES"
+
+class LignelistFragment : Fragment(){
+    private lateinit var lignes : ArrayList<Ligne>
+    private lateinit var rcvLignes : RecyclerView
+    private lateinit var ligneAdapter: LigneAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        lignes = arguments!!.getSerializable(ARG_LIGNES) as ArrayList<Ligne>
+    }
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ) :View?{
+        val rootView = inflater.inflate(R.layout.fragment_ligne_list, container,false)
+        this.rcvLignes = rootView.findViewById(R.id.f_ligne_list_rcv_ligne)
+        ligneAdapter = LigneAdapter(lignes)
+        this.rcvLignes.adapter = ligneAdapter
+        val linearLayoutManager = LinearLayoutManager(context)
+        this.rcvLignes.layoutManager = linearLayoutManager
+
+        val dividerItemDecoration = DividerItemDecoration(context, linearLayoutManager.orientation)
+        this.rcvLignes.addItemDecoration(dividerItemDecoration)
+
+        return rootView
+
+    }
+}
