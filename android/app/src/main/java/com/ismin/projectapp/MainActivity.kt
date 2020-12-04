35fe9addd9026c01f62cc306cbd993950a358152
vince <vincentbourel@laposte.net>
2020-12-04 15:27:08 +0100

    version fusion 1.1

--- android/app/src/main/java/com/ismin/projectapp/MainActivity.kt
+++ android/app/src/main/java/com/ismin/projectapp/MainActivity.kt
@@ -5,7 +5,15 @@ import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
+import android.widget.Toast
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.*
+import kotlin.collections.ArrayList
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(),LigneCreator,LigneShearchor{
