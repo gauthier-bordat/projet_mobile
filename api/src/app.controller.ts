62b1588bebc8669aed86ae890a45732c4d029fd8
vince <vincentbourel@laposte.net>
2020-12-04 22:43:04 +0100

    api update

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -7,12 +7,31 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getAll(): Promise<Ligne_traitee[]> {
-    return this.appService.getAll();
+    getLigne(@Query("favorie")favorie: string): Promise<Ligne_traitee[]> {
+      if(favorie==undefined){
+        return Promise.resolve(this.appService.getAll());
+      }
+      else{
+      return this.appService.getFavorie();}
+    }
+    @Get(':nom')
+    getbyname(@Param('nom') param: string): Promise<Ligne_traitee>{
+      return Promise.resolve(this.appService.getLignebyname(param));
+    }
+    @Get(':numero')
+    getbynumero(@Param('numero')param : string): Promise<Ligne_traitee>{
+      return Promise.resolve(this.appService.getLignebynum(param));
+    }
+    @Post(':nom')
+    ajouterFavorie(@Param('nom')param:string):Promise<Ligne_traitee>{
+      return Promise.resolve(this.appService.ajouterFavorie(param));
+    }
+    @Post('/search')
+    getLigneLike(@Body()param: Search): Promise<Ligne_traitee[]>{
+      return Promise.resolve(this.appService.getLignelike(param));
+    }
+    @Put()
+    addLigne(@Body() newLigne:Ligne_traitee):Promise<Ligne_traitee> | undefined {
+      return this.appService.addLigne(newLigne);
+    }
   }
-
-  @Put()
-  addLigne(@Body() newLigne:Ligne_traitee):Promise<Ligne_traitee> | undefined {
-    return this.appService.addLigne(newLigne);
-  }
-}
